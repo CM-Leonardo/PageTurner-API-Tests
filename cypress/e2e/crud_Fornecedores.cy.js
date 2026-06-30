e311c4c0be87d0721c48246c0c4f082162eba7e2
CM-Leonardo <leo_coneglian56@outlook.com>
2026-06-30 16:05:52 -0300

    editando o commit

--- cypress/e2e/crud_Fornecedores.cy.js
+++ cypress/e2e/crud_Fornecedores.cy.js
@@ -3,6 +3,7 @@ import { authentication } from "../functions/api/auth"
 import { user, editoras, messages, MESSAGES } from "../functions/utils/envVariaveis"
 import { expectBadRequest, expectForbidden, expectInternalServerError, expectSuccessfullyListEmpyt, expectUnauthorized } from "../functions/api/utils"
 
+
 describe('CRUD Editoras - API', () => {
     let acessTokenAdm
     let acessToken
